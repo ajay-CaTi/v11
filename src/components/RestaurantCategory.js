c5a577fd26d5e042af3bcfa7ea40c6bce64fc61d
ajay-CaTi <ajaykumarsingh110@gmail.com>
2023-09-26 15:44:25 +0530

    upto 013300 upto toggle of accordian simple

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
+  const [showItems, setShowItems] = useState(false);
+  const handleClick = () => {
+    console.log("click");
+    setShowItems(!showItems);
+  };
   console.log(data);
   return (
     <div>
       {/* Header  */}
 
       <div className=" bg-gray-50 shadow-lg p-4 w-10/12 mx-auto  my-2 rounded-lg border-black">
-        <div className="flex justify-between">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
           <span className="font-bold text-lg">
             {data.title} - ({data.itemCards.length})
           </span>
           <span>🔽</span>
         </div>
-        <ItemList items={data.itemCards} />
+        {showItems && <ItemList items={data.itemCards} />}
       </div>
 
       {/* Accordian Body  */}
